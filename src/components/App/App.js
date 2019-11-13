56185ef420587fbcf44d0f4e558d1dfb7d385e94
valentin gordienko <valentingordienko.dev@yandex.ru>
2019-11-13 22:10:39 +0300

    Creating App component

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameBoard from "../GameBoard";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <GameBoard/>
+    </div>
+  );
+}
+
+export default App;
